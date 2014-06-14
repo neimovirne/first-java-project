7e39b0b78d6d5eb14898ff143b5a7c4ca8507e37
neimovirne <neimovirne@gmail.com>
2014-06-14 03:17:48 +0300

    initial project version

---
+++ src/com/lesson2/Main.java
@@ -0,0 +1,12 @@
+package com.lesson2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Field field = new Field();
+        field.ereseField();
+        field.showField();
+    }
+}
+
